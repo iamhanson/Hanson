2a0175d20cc7f41706c22ba6c72cfc1b9a7a82dd
iamhanson <237905173@qq.com>
2014-10-24 16:10:01 +0800

    remove dao

--- dao/readfile.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-//获取文件之后打印
-exports.getUser=function(dir,backFunc){
-	fs.readFile(dir+'/dao/data/user.txt','utf8', function (err, logData) {
-		if (err) throw err;
-		backFunc.call(this,logData);
-	});
-}
--- dao/settings.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-//获取文件之后打印
-exports.settings=function(callBack){
-	fs.readFile(__dirname+'\\data\\settings.txt','utf8', function (err, logData) {
-		if (err) throw err;
-		callBack.call(this,logData);
-	});
-}
